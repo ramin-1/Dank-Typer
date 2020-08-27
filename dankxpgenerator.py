4673d423fbf560de29a68ab151d27a4268d12bb1
Pugaluffagus <erahul.ka@gmail.com>
2020-08-27 09:34:47 -0700

    Update dankxpgenerator.py
    Removed the gambling and selling commands, updated the search command. Typer now fake uses pinks, buys cookies, and searches the air. Recommended that you have 10+ lifesavers on you at all times because you can die from the air.

--- dankxpgenerator.py
+++ dankxpgenerator.py
@@ -6,53 +6,56 @@ import math
 keyboard = Controller()
 time.sleep(5)
 
+
 def enter():
     keyboard.press(Key.enter)
     keyboard.release(Key.enter)
 
+
 def beg():
     keyboard.type('pls beg')
     enter()
-    time.sleep(random.randint(2, 5))
+    time.sleep(random.randint(2, 4))
 
 
 def gamble():
-    keyboard.type('pls gamble 100')
+    keyboard.type('pls bet 100')
     enter()
-    time.sleep(random.randint(2, 5))
+    time.sleep(random.randint(1, 3))
 
 
 def slots():
     keyboard.type('pls slots 100')
     enter()
-    time.sleep(random.randint(2, 5))
+    time.sleep(random.randint(1, 3))
 
 
 def cookie():
-    keyboard.type('pls sell cookie')
-    enter()
-    time.sleep(random.randint(2, 5))
-    
+    use()
+
+
 def buy():
-    keyboard.type('pls buy cookie')
+    keyboard.type('pls buy coo')
     enter()
-    time.sleep(random.randint(2, 5))
+    time.sleep(random.randint(2, 4))
+
 
 def trivia():
     keyboard.type('pls trivia')
     enter()
     time.sleep(random.randint(1, 2))
-    trivia_num = random.randint(1,4)
+    trivia_num = random.randint(1, 4)
     if trivia_num == 1:
         keyboard.type('A')
     elif trivia_num == 2:
         keyboard.type('B')
     elif trivia_num == 3:
         keyboard.type('C')
-    else: 
+    else:
         keyboard.type('D')
     enter()
-    time.sleep(random.randint(2, 5))
+    time.sleep(random.randint(2, 4))
+
 
 def use():
     keyboard.type('pls use pink')
@@ -60,15 +63,17 @@ def use():
     time.sleep(random.randint(1, 2))
     keyboard.type('g')
     enter()
-    time.sleep(random.randint(2, 5))
+    time.sleep(random.randint(2, 4))
+
 
 def scout():
     keyboard.type('pls scout')
     enter()
     time.sleep(random.randint(1, 2))
-    keyboard.type('tree')
+    keyboard.type('air')
     enter()
-    time.sleep(random.randint(2, 5))
+    time.sleep(random.randint(2, 4))
+
 
 def pm():
     keyboard.type('pls pm')
@@ -88,7 +93,8 @@ def pm():
         keyboard.press('e')
         keyboard.release('e')
     enter()
-    time.sleep(random.randint(2, 5))
+    time.sleep(random.randint(3, 5))
+
 
 def full_cookie():
     cookie()
@@ -98,8 +104,8 @@ def full_cookie():
 
 
 phrases = ["damn", "coolio", "im rich af", "oop", "shiii", "dam it", "cmon man", "lol", "ripp",
-           "wtf", "crazy stuff", "gimme more coin", "bruh", "lmao", "what thee heck", "haha", "good bot", 
-           "xd","what do you want man?","wow","bad bot","rip the laptop","wow","why u doin this","grinding is tough man"]
+           "wtf", "crazy stuff", "gimme more coin", "bruh", "lmao", "what thee heck", "haha", "good bot",
+           "xd", "what do you want man?", "wow", "bad bot", "rip the laptop", "wow", "why u doin this", "grinding is tough man"]
 
 
 def phrase():
@@ -116,21 +122,26 @@ while x < 1000000:
         use()
         trivia()
         cookie()
-        slots()
+        # slots()
+        use()
         scout()
-        slots()
-        gamble()
-        cookie()
+        # slots()
+        buy()
+        # gamble()
+        use()
         pm()
         buy()
+        cookie()
         beg()
         use()
     elif num == 2:
+        use()
         trivia()
         use()
-        gamble()
+        # gamble()
         buy()
-        slots()
+        # slots()
+        cookie()
         use()
         scout()
         cookie()
@@ -139,79 +150,91 @@ while x < 1000000:
         beg()
         use()
     elif num == 3:
-        slots()
+        use()
+        # slots()
+        cookie()
         pm()
+        buy()
         trivia()
         cookie()
-        gamble()
+        # gamble()
         buy()
         scout()
+        use()
         beg()
         use()
-        slots()
+        # slots()
         buy()
     elif num == 4:
-        gamble()
+        # gamble()
         scout()
         buy()
-        slots()
+        # slots()
         cookie()
         pm()
         buy()
-        gamble()
+        # gamble()
         use()
         trivia()
         cookie()
         beg()
-        buy()
+        # gamble()
     elif num == 5:
+        buy()
         cookie()
         use()
         scout()
         cookie()
         pm()
-        buy()
+        use()
+        # slots()
         beg()
-        trivia()
-        cookie()
-        slots()
         buy()
-        gamble()
+        cookie()
+        trivia()
+        use()
+        # slots()
+        buy()
+        # gamble()
         buy()
     elif num == 6:
         buy()
         pm()
         cookie()
-        slots()
-        gamble()
+        # slots()
+        use()
+        # gamble()
+        cookie()
         trivia()
+        buy()
         beg()
         buy()
         scout()
         use()
-        gamble()
-        cookie()
+        # gamble()
+        use()
     else:
         cookie()
         beg()
+        use()
         scout()
         use()
-        slots()
+        # slots()
         buy()
         pm()
         use()
         trivia()
-        cookie()
-        gamble()
+        use()
+        # gamble()
         buy()
-    if x == cook_buy:
-        cookie_num = random.randint(340, 360)
-        keyboard.type(f'pls buy cookie {cookie_num}')
-        keyboard.press(Key.enter)
-        keyboard.release(Key.enter)
-        cook_buy += random.randint(90, 110)
+    # if x == cook_buy:
+    #     cookie_num = random.randint(340, 360)
+    #     keyboard.type(f'pls buy cookie {cookie_num}')
+    #     keyboard.press(Key.enter)
+    #     keyboard.release(Key.enter)
+    #     cook_buy += random.randint(90, 110)
     phrase()
-    time.sleep(random.randint(2, 5))
+    time.sleep(random.randint(1, 3))
     x += 1
     
 #No Selling
